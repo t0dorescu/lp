e07edb5a9a6e67ff2050b002c344043a4aad9349
t0rescu <todorescu.consulting@gmail.com>
2022-07-10 16:53:27 +0300

    changes

--- php/lp-footer.php
+++ php/lp-footer.php
@@ -157,3 +157,15 @@
         }
     })
 </script>
+
+<?php if (!$isDev) : ?>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-122326504-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-122326504-1');
+  </script>
+<?php endif ?>
