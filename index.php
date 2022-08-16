cc0cb17ef5c947669f804bff81bc2b087a211886
Tudor Todorescu <todorescu.consulting@gmail.com>
2022-08-16 03:38:56 +0300

    .

--- index.php
+++ index.php
@@ -77,7 +77,7 @@
         <div class="row gx-0">
           <div class="col-lg-12 d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="200">
             <div class="content" style="opacity: .9;">
-              <h3 style="margin-top: 40px;">Tudor's Tale</h3>
+              <h3 style="margin-top: 10px;">Tudor's Tale</h3>
 
               <p>Most developers have serious confidence issues, <strong>Tudor was no different</strong>. He did everything right, but still he was stuck in junior jobs, barely getting time to enjoy his 20s</p>
               
@@ -93,15 +93,18 @@
                 <source src="<?= root().'assets/video/revolut.mp4' ?>" type="video/mp4">
               </video>
                 
-              <p><br>He finally cracked it, and it was surprisingly easy once he got it. It even works without having FAANG on your resume. His approach is so unique, he instantly made him stand-out. (even if he never worked an actual senior job/project before)</p>
+              <p>He finally cracked it, and it was surprisingly easy once he got it. It even works without having <strike>FAANG</strike> on your resume. His approach is so unique, he instantly made him stand-out. (even if he never worked an actual senior job/project before)</p>
 
               <p>From a sad stuck shmuk, into a sad free man overnight (almost)... oh yeah, btw, happiness comes from other places; that's a different story.</p>
 
               <img src="<?= root().'assets/img/zoom.jpg' ?>" style="width: 100%;" />
               
-              <p>He's not a life-coach, therapist or sifu. Tudor earns his living doing high-end vue3 projects<br> <a href="https://www.linkedin.com/in/tudortodorescu/" target="_blank">(latest on Linkedin <i class="bi bi-linkedin"></i>)</a>. <br><br><b>Money is empty, sharing is fun. Tudor loves to hear all about his students success, and be a part of their journey.</b>
+              <p>He's not a life-coach, therapist or sifu. Tudor earns his living doing high-end vue3 projects<br> <a href="https://www.linkedin.com/in/tudortodorescu/" target="_blank">( latest on Linkedin <i class="bi bi-linkedin"></i> )</a>. <br><br><b>Money is empty, sharing is fun. Tudor loves to hear all about his students success, and be a part of their journey.</b>
 
-              <p>Want more? Write down why you want a change in your career and email <a href="mailto:adina@todorescu.com">adina@todorescu.com</a> with your why. <br><br><i>She will forward your story to Tudor, and he'll prepare a special training (in video format).</i></p>
+                <br><br>
+              <img src="<?= root().'assets/img/tour.gif' ?>" style="width: 100%;" />
+              
+              <p><b>Thirsty for more?</b> Write down your why in detail, then man-up and email <a href="mailto:adina@todorescu.com">adina@todorescu.com</a>. <br><br><i>She will forward your story to Tudor, and he'll prepare an exclusive training video (limited).</i></p>
 
               <a 
                 href="mailto:adina@todorescu.com"
