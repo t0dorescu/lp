7bbf928dee2427e2976cc0d3fce107b8caf4fdef
Tudor Todorescu <todorescu.consulting@gmail.com>
2022-08-31 00:48:08 +0300

    .

---
+++ give-away.php
@@ -0,0 +1,96 @@
+<?php
+include $_SERVER['DOCUMENT_ROOT'] . "/api/light.php";
+$optin_page = true;
+
+$title = "We're giving away $43,890. Want some of it ?";
+include php_root() . 'php/lp-header.php';
+?>
+
+<main id="main">
+    <section class="hero" style="margin-top: 0;">
+        <div class="container main-container" style="margin-top: 0;">
+            <div class="row">
+                <div class="col-lg-6 col-12" style="text-align: left;">
+                    <h1 style="font-size: 1.7rem;">
+                        We're giving away $43,890. Want some of it ?
+                    </h1>
+                    <h2>
+                        Gonna split it up between 21 people over 21 days, $1791 + $299 each.
+                    </h2>
+                    <br>
+                    <h3 style="color: var(--primary-color); font-weight: bold;">
+                        **HERE'S THE DEAL**
+                    </h3>
+                    <p style="color: white;">
+                        We truly believe the "WebDev Employeed Roadmap" is the greatest program ever created, and we want to prove it and have some fun. So we're doing something a little crazy...
+                        <br><br>
+                        We're giving away $43,890 in actual value over the next 21 days.
+                        <br><br>
+                        Every single day, one new person will get:
+                        <br><br>
+                        A zoom session with Tudor worth $299, 9x of our web-dev courses worth $1791<br>
+                        One new person. Every day. 21 days.
+                    </p>
+                    <h3 style="color: var(--primary-color); font-weight: bold;">
+                        **WHY WE ARE DOING THIS**
+                    </h3>
+                    <p style="color: white;">
+                        1. Almost all of you will become advocates after receiving this and spread the word about us better than we can.
+                        <br><br>
+                        2. Many people will want more help from us and join the "WebDev Employeed Roadmap"
+                        <br><br>
+                        3. We make $43,890 by first giving $43,890 of value to the market
+                        <br><br>
+                        4. Helping devs at their start is fulfilling and we get better everytime I do it
+                        <br><br>
+                        5. This is way more fun than posting a 2 long boring hours of technical content.
+                    </p>
+                    <h3 style="color: var(--primary-color); font-weight: bold;">
+                        **WHY YOU SHOULD DO THIS**
+                    </h3>
+                    <p style="color: white;">
+                        1. So you don't accidentally waste half of year going down the wrong path.
+                        <br><br>
+                        2. So you can get inspired and break out of a rut
+                        <br><br>
+                        3. Personal advice from Tudor is better than going to the spa.
+                        <br><br>
+                        4. You'd be insane not to
+                    </p>
+                    <h3 style="color: var(--primary-color); font-weight: bold;">
+                        **HOW TO QUALIFY FOR A SPOT**
+                    </h3>
+                    <p style="color: white;">
+                        - Are you a human?<br>
+                        - Need a web developer job?<br>
+                        - Need a road-map?<br>
+                        - Need a guiding hand?<br>
+                        - Stuck and can't figure out why?<br>
+                        - You are not super annoying?
+                        <br><br>
+                        CONGRATULATIONS YOU QUALIFY
+                    </p>
+                    <h3 style="color: var(--primary-color); font-weight: bold;">
+                        **WHAT TO DO NEXT**
+                    </h3>
+                    <p style="color: white;">
+                        Send an email to our address: <a href="mailto:want@todorescu.com">want@todorescu.com</a>
+                        <br><br>
+                        Our team will reply and give you a number between 1 and 21.
+                        <br><br>
+                        Your number will give you lifetime access to our entire product suite and a private zoom call with Tudor worth $299, to take place on the date that comes with your number.
+                        <br><br>
+                        I recommend you ask for your number quickly to avoid being too far down the queue.
+                        <br><br>
+                        Lets get you WebDev Employeed!
+                        <br><br>
+                        <img src="<?= root() . 'assets/img/tudor-todorescu.jpg' ?>" style="width: 30px;" />
+
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php include php_root() . 'php/lp-footer.php'; ?>
--- php/lp-footer.php
+++ php/lp-footer.php
@@ -26,6 +26,14 @@
   </a>
 <?php endif ?>
 
+<?php if ($optin_page) : ?>
+    <ul style="text-align: center;">
+      <li style="display: inline;padding-right: 15px;"><a href="<?= root().'terms-of-service' ?>" target="_blank">Terms of service</a></li>
+      <li style="display: inline;padding-right: 15px;"><a href="<?= root().'privacy-policy' ?>" target="_blank">Privacy policy</a></li>
+      <li style="display: inline;padding-right: 15px;"><a href="<?= root().'cookie-policy' ?>" target="_blank">Cookie policy</a></li>
+    </ul>
+<?php endif ?>
+
 <!-- Vendor JS Files -->
 <script src="<?= root().'assets/vendor/bootstrap/js/bootstrap.bundle.min.js'.v_lp() ?>"></script>
 <script src="<?= root().'assets/vendor/php-email-form/php-email-form.js'.v_lp() ?>"></script>
@@ -41,54 +49,56 @@
 <script src="<?= root().'assets/js/utils/forms.js'.v_lp() ?>"></script>
 <script src="<?= root().'assets/js/services/api.js'.v_lp() ?>"></script>
 
-<!-- Cookie consent -->
-<script src="<?= root().'assets/vendor/cookieconsent/cookieconsent.js'.v_lp() ?>"></script>
-<script>
-    initCookieConsent().run({
-        languages: {
-        'en': {
-            consent_modal: {
-              title: 'Cookie policy',
-              description: 'This website uses cookies to understand how you interact with it. <a href="<?= root()."cookie-policy" ?>" target="_blank">Read more</a>',
-              primary_btn: {
-                  text: 'Accept all',
-                  role: 'accept_all'
-              },
-              secondary_btn: {
-                  text: 'Settings',
-                  role: 'settings'
-              }
-            },
-            settings_modal: {
-              title: 'Cookie preferences',
-              save_settings_btn: 'Save settings',
-              accept_all_btn: 'Accept all',
-              blocks: [
-                {
-                    title: 'Essential cookies',
-                    description: 'These cookies are strictly necessary to enable core functionality such as security, network management, your cookie preferences and accessibility'
-                }, {
-                    title: 'Performance and Functionality Cookies',
-                    description: 'Performance and Functionality Cookies help us remember that an user had logged in previously',
-                }, {
-                    title: 'Marketing cookies',
-                    description: 'Marketing Cookies enable us to identify future prospects and personalize sales and marketing interactions.',
-                }, {
-                    title: 'Analytics and Customization Cookies',
-                    description: 'These cookies collect information that is used to help us understand how your platform is being used or how effective our marketing campaigns are',
-                }, {
-                    title: 'Social Media Cookies',
-                    description: 'These cookies are used when users log in with Facebook, Twitter or Google+',
-                }, {
-                    title: 'Third Party Cookies',
-                    description: 'Third party service providers control many of the performance and functionality, advertising, marketing and analytics cookies',
+<?php if (!$optin_page) : ?>
+  <!-- Cookie consent -->
+  <script src="<?= root().'assets/vendor/cookieconsent/cookieconsent.js'.v_lp() ?>"></script>
+  <script>
+      initCookieConsent().run({
+          languages: {
+          'en': {
+              consent_modal: {
+                title: 'Cookie policy',
+                description: 'This website uses cookies to understand how you interact with it. <a href="<?= root()."cookie-policy" ?>" target="_blank">Read more</a>',
+                primary_btn: {
+                    text: 'Accept all',
+                    role: 'accept_all'
+                },
+                secondary_btn: {
+                    text: 'Settings',
+                    role: 'settings'
                 }
-              ]
-            }
-        }
-        }
-    })
-</script>
+              },
+              settings_modal: {
+                title: 'Cookie preferences',
+                save_settings_btn: 'Save settings',
+                accept_all_btn: 'Accept all',
+                blocks: [
+                  {
+                      title: 'Essential cookies',
+                      description: 'These cookies are strictly necessary to enable core functionality such as security, network management, your cookie preferences and accessibility'
+                  }, {
+                      title: 'Performance and Functionality Cookies',
+                      description: 'Performance and Functionality Cookies help us remember that an user had logged in previously',
+                  }, {
+                      title: 'Marketing cookies',
+                      description: 'Marketing Cookies enable us to identify future prospects and personalize sales and marketing interactions.',
+                  }, {
+                      title: 'Analytics and Customization Cookies',
+                      description: 'These cookies collect information that is used to help us understand how your platform is being used or how effective our marketing campaigns are',
+                  }, {
+                      title: 'Social Media Cookies',
+                      description: 'These cookies are used when users log in with Facebook, Twitter or Google+',
+                  }, {
+                      title: 'Third Party Cookies',
+                      description: 'Third party service providers control many of the performance and functionality, advertising, marketing and analytics cookies',
+                  }
+                ]
+              }
+          }
+          }
+      })
+  </script>
+<?php endif ?>
 
 </body>
 </html>
